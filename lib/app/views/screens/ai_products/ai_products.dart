bbf69d5927b5c45cd25205f20689f6c095988535
Shakib Al Hasan <mdshakib@joinventureai.com>
2026-01-11 12:27:18 +0600

    refactor: Remove unnecessary documentation comments from ai_products.dart

--- lib/app/views/screens/ai_products/ai_products.dart
+++ lib/app/views/screens/ai_products/ai_products.dart
@@ -1,9 +1,2 @@
-/// Export file for easy imports
-///
-/// Usage:
-/// ```dart
-/// import 'package:floor_bot_mobile/app/views/screens/ai_products/ai_products.dart';
-/// ```
-
 export 'ai_products_screen.dart';
 export '../../../controllers/ai_products_controller.dart';
